39b21bd1c79d501fe8bcbcc92c13c6e735b346b8
Parirajan Ravindran <39503659+pjravindran@users.noreply.github.com>
2020-11-13 15:57:30 -0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,1 +1,9 @@
-aws s3 cp ASG-UpdatePolicy-RollingUpdate.json s3://bogo-aws/alb/ASG-UpdatePolicy-RollingUpdate.json 
+#Upload the json file for creating the stack - This can be done by this method or via Console
+
+aws s3 cp awsasgcf.json s3://{bucketname}/awsasgcf.json
+
+
+aws cloudformation create-stack --stack-name CC615x-stack \
+--template-url https://s3.amazonaws.com/{bucketname}/awsasgcf.json \
+--parameters ParameterKey=KeyName,ParameterValue=ASG ParameterKey=VpcId,ParameterValue={vpc.id} \
+ParameterKey=Subnets,ParameterValue={subnet-1}\\,{subnet-2} --capabilities CAPABILITY_IAM
